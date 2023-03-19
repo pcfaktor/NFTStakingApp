c154370014a0636b900104eb8c14e8eacd0c1038
a.savich <a.savich@sparklab.by>
2023-03-19 20:10:11 +0300

    feat: implement the minting UI

--- src/components/WalletContextProvider.tsx
+++ src/components/WalletContextProvider.tsx
@@ -2,16 +2,17 @@ import { FC, ReactNode, useMemo } from "react"
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react"
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"
 import { clusterApiUrl } from "@solana/web3.js"
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets"
+import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets"
 require("@solana/wallet-adapter-react-ui/styles.css")
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
     const url = useMemo(() => clusterApiUrl("devnet"), [])
     const phantom = new PhantomWalletAdapter()
+    const solflare = new SolflareWalletAdapter()
 
     return (
         <ConnectionProvider endpoint={url}>
-            <WalletProvider wallets={[phantom]}>
+            <WalletProvider wallets={[phantom,solflare]}>
                 <WalletModalProvider>
                     {children}
                 </WalletModalProvider>
